34c31c1865a4425ed2a593b1de932c09cca0097b
yanglbme <szuyanglb@outlook.com>
2020-11-05 09:23:03 +0800

    feat: use fake-useragent package

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM python:3-slim AS builder
 ADD . /app
 WORKDIR /app
 
-RUN pip install --target=/app requests
-RUN pip install --target=/app rsa
+COPY requirements.txt requirements.txt
+
+RUN pip install --target=/app -r requirements.txt
 
 FROM gcr.io/distroless/python3-debian10
 COPY --from=builder /app /app
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import base64
 import os
-import random
 import re
 
 import requests
 import requests.packages.urllib3
 import rsa
+from fake_useragent import UserAgent
 
 PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDIrn+WB2Yi4ABAL5Tq6E09tumY
@@ -13,45 +13,6 @@ qVTFdpU01kCDUmClczJOCGZriLNMrshmN9NJxazpqizPthwS1OIK3HwRLEP9D3GL
 7gCnvN6lpIpoVwppWd65f/rK2ewv6dstN0fCmtVj4WsLUchWlgNuVTfWljiBK/Dc
 YkfslRZzCq5Fl3ooowIDAQAB
 -----END PUBLIC KEY-----"""
-
-USER_AGENTS = [
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; "
-    "SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; "
-    "SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; "
-    "Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; "
-    "Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; "
-    ".NET CLR 2.0.50727; Media Center PC 6.0)",
-    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; "
-    "Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; "
-    ".NET CLR 3.5.30729; .NET CLR 3.0.30729; "
-    ".NET CLR 1.0.3705; .NET CLR 1.1.4322)",
-    "Mozilla/4.0 (compatible; MSIE 7.0b; "
-    "Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; "
-    "InfoPath.2; .NET CLR 3.0.04506.30)",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) "
-    "AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) "
-    "Arora/0.3 (Change: 287 c9dfb30)",
-    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ "
-    "(KHTML, like Gecko, Safari/419.3) Arora/0.6",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; "
-    "rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) "
-    "Gecko/20080705 Firefox/3.0 Kapiko/3.0",
-    "Mozilla/5.0 (X11; Linux i686; U;) "
-    "Gecko/20070322 Kazehakase/0.4.5",
-    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) "
-    "Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 "
-    "(KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) "
-    "AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
-    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) "
-    "Presto/2.9.168 Version/11.52",
-]
 
 requests.packages.urllib3.disable_warnings()
 
@@ -64,6 +25,7 @@ class Action:
     def __init__(self, username, password, repo,
                  branch='master', directory='', https='true'):
         self.session = requests.session()
+        self.ua = UserAgent()
         self.username = username
         self.password = password
         self.repo = repo
@@ -87,7 +49,7 @@ class Action:
                       'q=0.9,image/webp,image/apng,*/*;'
                       'q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Host': 'gitee.com',
-            'User-Agent': random.choice(USER_AGENTS)
+            'User-Agent': self.ua.random
         }
         try:
             resp = self.session.get(url=login_index_url,
@@ -99,7 +61,7 @@ class Action:
                 'Referer': 'https://gitee.com/login',
                 'X-Requested-With': 'XMLHttpRequest',
                 'X-CSRF-Token': csrf_token,
-                'User-Agent': random.choice(USER_AGENTS)
+                'User-Agent': self.ua.random
             }
             self.session.post(url=check_login_url,
                               headers=headers,
@@ -142,7 +104,7 @@ class Action:
                 'Referer': pages_url,
                 'X-Requested-With': 'XMLHttpRequest',
                 'X-CSRF-Token': csrf_token,
-                'User-Agent': random.choice(USER_AGENTS)
+                'User-Agent': self.ua.random
             }
             form_data = {
                 'branch': self.branch,
