b4252b90e333914452def4bc23306a4a62ddfe0f
yanglbme <szuyanglb@outlook.com>
2020-11-17 23:36:21 +0800

    style: rename variables

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import sys
 from app.action import Action
 
 if __name__ == '__main__':
-    input_username = os.environ.get('INPUT_GITEE-USERNAME')
-    input_password = os.environ.get('INPUT_GITEE-PASSWORD')
-    input_repo = os.environ.get('INPUT_GITEE-REPO')
-    input_branch = os.environ.get('INPUT_BRANCH')
-    input_directory = os.environ.get('INPUT_DIRECTORY')
-    input_https = os.environ.get('INPUT_HTTPS')
-    action = Action(input_username, input_password, input_repo,
-                    input_branch, input_directory, input_https)
+    username = os.environ.get('INPUT_GITEE-USERNAME')
+    password = os.environ.get('INPUT_GITEE-PASSWORD')
+    repo = os.environ.get('INPUT_GITEE-REPO')
+    branch = os.environ.get('INPUT_BRANCH')
+    directory = os.environ.get('INPUT_DIRECTORY')
+    https = os.environ.get('INPUT_HTTPS')
+    action = Action(username, password, repo,
+                    branch, directory, https)
     try:
         action.run()
         print('rebuild Gitee Pages successfully')
