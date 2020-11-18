bd8c7d4858c318a248ab3ebfebbd0469aa6e6c5a
yanglbme <szuyanglb@outlook.com>
2020-11-18 10:25:27 +0800

    feat: add get_input function

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ import sys
 
 from app.action import Action
 
+
+def get_input(name: str, required: bool = True) -> str:
+    name = f'INPUT_{name.replace(" ", "_").upper()}'
+    val = os.environ.get(name) or ''
+    if required and not val:
+        raise Exception(f'Input required and not supplied: {name}')
+    return val.strip()
+
+
 if __name__ == '__main__':
-    username = os.environ.get('INPUT_GITEE-USERNAME')
-    password = os.environ.get('INPUT_GITEE-PASSWORD')
-    repo = os.environ.get('INPUT_GITEE-REPO')
-    branch = os.environ.get('INPUT_BRANCH')
-    directory = os.environ.get('INPUT_DIRECTORY')
-    https = os.environ.get('INPUT_HTTPS')
-    action = Action(username, password, repo,
-                    branch, directory, https)
+    username = get_input('gitee-username')
+    password = get_input('gitee-password')
+    repo = get_input('gitee-repo')
+    branch = get_input('branch', required=False)
+    directory = get_input('directory', required=False)
+    https = get_input('https', required=False)
+    action = Action(username, password, repo, branch, directory, https)
     try:
         action.run()
         print('rebuild Gitee Pages successfully')
