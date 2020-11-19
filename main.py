96efd7b1d660817a5e11fd27ffcc05d86fa11815
yanglbme <szuyanglb@outlook.com>
2020-11-19 16:55:34 +0800

    feat: use actions toolkit

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-import os
-import sys
+from actions_toolkit import core
 
 from app.action import Action
 
-
-def get_input(name: str, required: bool = True) -> str:
-    name = f'INPUT_{name.replace(" ", "_").upper()}'
-    val = os.environ.get(name) or ''
-    if required and not val:
-        raise Exception(f'Input required and not supplied: {name}')
-    return val.strip()
-
-
 if __name__ == '__main__':
-    username = get_input('gitee-username')
-    password = get_input('gitee-password')
-    repo = get_input('gitee-repo')
-    branch = get_input('branch', required=False)
-    directory = get_input('directory', required=False)
-    https = get_input('https', required=False)
+    username = core.get_input('gitee-username')
+    password = core.get_input('gitee-password')
+    repo = core.get_input('gitee-repo')
+    branch = core.get_input('branch', required=False)
+    directory = core.get_input('directory', required=False)
+    https = core.get_input('https', required=False)
     action = Action(username, password, repo, branch, directory, https)
     try:
         action.run()
-        print('rebuild Gitee Pages successfully')
+        core.info('rebuild Gitee Pages successfully')
     except Exception as e:
-        print(f'::error::{e}')
-        sys.exit(1)
+        core.set_failed(str(e))
