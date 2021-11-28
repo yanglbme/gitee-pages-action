44ea6ceeee1692deb81f75196b7119f7f94f8bc5
yanglbme <szuyanglb@outlook.com>
2021-11-28 11:44:58 +0800

    chore: update test case

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os
 
 from actions_toolkit import core
 
+from app import log
 from app.action import Action
 
 os.environ['INPUT_GITEE-USERNAME'] = 'yanglbme'
@@ -9,10 +10,13 @@ os.environ['INPUT_GITEE-PASSWORD'] = '***'
 os.environ['INPUT_GITEE-REPO'] = 'yanglbme/reading'
 os.environ['INPUT_BRANCH'] = 'main'
 
-username = core.get_input('gitee-username', required=True)
-password = core.get_input('gitee-password', required=True)
-repo = core.get_input('gitee-repo', required=True)
-branch = core.get_input('branch')
+try:
+    username = core.get_input('gitee-username', required=True)
+    password = core.get_input('gitee-password', required=True)
+    repo = core.get_input('gitee-repo', required=True)
+    branch = core.get_input('branch')
 
-action = Action(username, password, repo, branch)
-action.run()
+    action = Action(username, password, repo, branch)
+    action.run()
+except Exception as e:
+    log.set_failed(str(e))
